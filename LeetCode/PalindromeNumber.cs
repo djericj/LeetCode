53f9ac29c653c3ed082560a44447865cbd4dfb64
Eric Rydberg <eric.rydberg@outlook.com>
2022-05-02 23:25:07 -0500

    added #10 Palindrome Number

---
+++ LeetCode/PalindromeNumber.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode
+{
+    /// <summary>
+    /// Solution for LeetCode 9. Palindrome Number
+    /// https://leetcode.com/problems/palindrome-number/
+    /// </summary>
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            var s = x.ToString();
+            var start = 0;
+            var end = s.Length - 1;
+            while (true)
+            {
+                if (start >= end)
+                    break;
+                if (s[start] == s[end])
+                {
+                    start++;
+                    end--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace LeetCode
+﻿namespace LeetCode
 {
     internal class Program
     {
@@ -15,54 +10,6 @@ namespace LeetCode
 
         public void Run()
         {
-            Console.WriteLine(MyAtoi("21474836460"));
-        }
-
-        public int MyAtoi(string s)
-        {
-            var neg = false;
-            var pos = false;
-            var startIndex = 0;
-            var integer = "";
-            int result = 0;
-            if (s.Length == 0) return 0;
-            s = s.TrimStart();
-            if (s.Length > 1)
-            {
-                neg = s.StartsWith("-");
-                pos = s.StartsWith("+");
-            }
-            if (neg || pos) startIndex++;
-            else if (!neg && !pos) pos = true;
-            var regex = new Regex("^[0-9]+$");
-            for (int i = startIndex; i < s.Length; i++)
-            {
-                if (regex.IsMatch(s[i].ToString()))
-                {
-                    integer += s[i];
-                }
-                else
-                {
-                    break;
-                }
-            }
-            if (int.TryParse(integer.ToString(), out result))
-            {
-                if (result >= int.MinValue && result <= int.MaxValue)
-                {
-                    if (neg) result = result * -1;
-                    return result;
-                }
-            }
-            else
-            {
-                if (!string.IsNullOrWhiteSpace(integer))
-                {
-                    if (neg) result = int.MinValue;
-                    if (pos) result = int.MaxValue;
-                }
-            }
-            return result;
         }
     }
 }
