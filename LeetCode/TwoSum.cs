f058fd10970e224817317ca00654841b281e5eba
Eric Rydberg <eric.rydberg@outlook.com>
2022-05-05 09:11:30 -0500

    added solution for LeetCode 167 Two Sum II

--- LeetCode/TwoSum.cs
+++ LeetCode/TwoSum.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace LeetCode
 {
-    /// <summary>
-    /// Solution for LeetCode 1. Two Sum
-    ///
-    /// </summary>
     public class TwoSumSolution
     {
+        /// <summary>
+        /// Solution for LeetCode 1. Two Sum
+        /// https://leetcode.com/problems/two-sum/
+        /// </summary>
         public int[] TwoSum(int[] nums, int target)
         {
             int[] ans = new int[1];
@@ -31,5 +31,24 @@ namespace LeetCode
             }
             return ans;
         }
+
+        /// <summary>
+        /// Solution for LeetCode 167. Two Sum II
+        /// https://leetcode.com/problems/two-sum/
+        /// </summary>
+        public int[] TwoSum2(int[] numbers, int target)
+        {
+            var l = 0;
+            var r = numbers.Length - 1;
+            while (true)
+            {
+                var sum = numbers[l] + numbers[r];
+                if (sum > target)
+                    r--;
+                if (sum < target)
+                    l++;
+                if (sum == target)
+                    return new int[] { l + 1, r + 1 };
+            }
+        }
     }
-}
